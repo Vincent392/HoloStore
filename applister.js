4ed76f76be0f223f44bead0e3fab079db05101e8
Vincent392 <90470156+Vincent392@users.noreply.github.com>
2024-01-05 15:38:27 +0000

    Update applister.js

--- applister.js
+++ applister.js
@@ -12,20 +12,23 @@ document.addEventListener('DOMContentLoaded', async function () {
 
     data.forEach(app => {
   const button = document.createElement('button');
-  button.innerText = app.label;
 
   const image = document.createElement('img');
   image.src = app.imagefile;
   image.alt = app.label;
 
+  const label = document.createElement('span');
+  label.innerText = app.label;
+
   button.appendChild(image);
+  button.appendChild(label);
 
   button.addEventListener('click', function () {
     window.location.href = app.page; // Navigate to the specified HTML page
   });
 
   appButtonsContainer.appendChild(button);
-    });
+});
   } catch (error) {
     console.error('Error fetching or processing apps.json:', error);
   }
